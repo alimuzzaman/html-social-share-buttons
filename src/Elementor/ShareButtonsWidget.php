d969189eb525cfb29bfba950797cc61ba69d5fa7
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2025-09-27 23:27:22 +0600

    PHASE2-Elementor: Add network selection control to ShareButtonsWidget

--- src/Elementor/ShareButtonsWidget.php
+++ src/Elementor/ShareButtonsWidget.php
@@ -24,7 +24,34 @@ class ShareButtonsWidget extends Widget_Base {
     }
 
     protected function _register_controls() {
-        // Controls will be added in later tasks
+        $this->start_controls_section(
+            'section_networks',
+            [
+                'label' => __( 'Social Networks', 'html-social-share' ),
+            ]
+        );
+
+        $this->add_control(
+            'networks',
+            [
+                'label' => __( 'Networks', 'html-social-share' ),
+                'type' => Controls_Manager::SELECT2,
+                'multiple' => true,
+                'options' => [
+                    'facebook' => __( 'Facebook', 'html-social-share' ),
+                    'twitter' => __( 'Twitter', 'html-social-share' ),
+                    'linkedin' => __( 'LinkedIn', 'html-social-share' ),
+                    'pinterest' => __( 'Pinterest', 'html-social-share' ),
+                    'reddit' => __( 'Reddit', 'html-social-share' ),
+                    'whatsapp' => __( 'WhatsApp', 'html-social-share' ),
+                    'email' => __( 'Email', 'html-social-share' ),
+                ],
+                'default' => [ 'facebook', 'twitter', 'linkedin' ],
+                'label_block' => true,
+            ]
+        );
+
+        $this->end_controls_section();
     }
 
     protected function render() {
