bf77c497ecc24e3b12f95b91c5969f677413b92d
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2025-09-27 22:52:17 +0600

    PHASE1-108: Add IconRegistry unit tests

---
+++ tests/unit/test-iconregistry.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/../../src/bootstrap.php';
+
+$reg = new HtmlSocialShare\IconRegistry();
+
+// initial list should contain at least 'twitter' from constructor
+$list = $reg->listIcons();
+if (!in_array('twitter', $list)) {
+    echo "FAIL: initial icons missing twitter\n";
+    exit(1);
+}
+
+$reg->registerIcon('foo/bar.svg', '<svg></svg>');
+if (!$reg->hasIcon('foo/bar.svg')) {
+    echo "FAIL: did not register icon\n";
+    exit(1);
+}
+
+$svg = $reg->getIcon('foo/bar.svg');
+if (strpos($svg, '<svg') === false) {
+    echo "FAIL: getIcon returned invalid svg\n";
+    exit(1);
+}
+
+echo "IconRegistry PASS\n";
+exit(0);
