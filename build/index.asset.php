fad335e6af9180160ffe2105692c19af6a348726
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2025-09-30 16:29:12 +0600

    build: update build assets after fixing admin CSS loading

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5a4ae18362dc4bc035f0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '25830fa2992256048e1d');
